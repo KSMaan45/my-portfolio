8e7d242b7f062f077d39048d341a3ff7965a4e44
K S Maan <86910011+KSMaan45@users.noreply.github.com>
2026-05-05 21:20:49 +0530

    Improved theme selection with remember last theme choice

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,16 +3,24 @@ import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
+
     if (storedTheme === "dark") {
-      localStorage.setItem("theme", "light");
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    } else if (storedTheme === "light") {
+      document.documentElement.classList.remove("dark");
       setIsDarkMode(false);
     } else {
-      setIsDarkMode(true);
-      document.documentElement.classList.add("dark");
+      const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+      if (prefersDark) {
+        document.documentElement.classList.add("dark");
+        setIsDarkMode(true);
+      }
     }
   }, []);
 
@@ -33,7 +41,7 @@ export const ThemeToggle = () => {
       onClick={toggleTheme}
       className={cn(
         "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outlin-hidden"
+        "focus:outline-none"
       )}
     >
       {isDarkMode ? (
